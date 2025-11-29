5ed8c090476f9aba8a196ebcba0bb65138e1230d
Graeme Doherty <graeme.doherty@gmail.com>
2025-11-29 13:47:17 +0000

    Initial Commit

---
+++ SleepShield/SleepShieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepShieldApp.swift
+//  SleepShield
+//
+//  Created by Graeme Doherty on 29/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct SleepShieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
